4e2acca47f7a17fe81a3b025802f2dcf2552e7b0
Sean Suwanaloet <suwanaloet@wisc.edu>
2020-07-23 16:25:23 -0700

    9.1

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log('Hello Node!');
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+console.log(profileDataArgs);
+
+// Notice the lack of parentheses around the `profileDataArr` parameter?
+const printProfileData = profileDataArr => {
+    console.log('================');
+
+    profileDataArr.forEach(profileItem => console.log(profileItem));
+    for (let i = 0; i < profileDataArr.length; i += 1) {
+        console.log(profileDataArr[i]);
+    }
+};
+
+printProfileData(profileDataArgs);
